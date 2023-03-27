8cc5d6cffcd55434a78fbc5411040afd13f4f85d
ESHAN GUPTA <es66han66@gmail.com>
2023-03-27 19:18:33 +0530

    Call and apply usage and comparison in performance

---
+++ call_apply_usage_and_comparison/usage_and_comparison.js
@@ -0,0 +1,28 @@
+function a(obj1, obj2){
+    let a = obj1.name
+    let b = obj2.name
+}
+
+console.time("first")
+for(let i = 0;i<=10000000; i++){
+    a.apply(this, [{
+        name: "eshan"
+    },
+    {
+        name: "eshan"
+    }
+    ])
+}
+console.timeEnd("first")
+
+console.time("second")
+for(let i = 0;i<=10000000; i++){
+    a.call(this, {
+        name: "eshan"
+    },
+    {
+        name: "eshan"
+    }
+    )
+}
+console.timeEnd("second")
