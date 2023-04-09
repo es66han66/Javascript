0688618432dc730eaca9c60ee4b54f8c9d2eb57f
ESHAN GUPTA <es66han66@gmail.com>
2023-04-09 22:41:45 +0530

    parse int output

---
+++ Output_questions/parseInt.js
@@ -0,0 +1,9 @@
+console.log(parseInt("7"))
+
+console.log(parseInt("7*6"))
+
+console.log(parseInt("7Eshan"))
+
+/**
+ * If we pass string and number combination to parseInt, what parseInt does is, it check at which position wrong datatype is getting started, if value before the wrong datatype is a valid number, it will return the valid number.
+ */
