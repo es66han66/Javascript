89d55068593f18bf9a82a304280510997884481d
ESHAN GUPTA <es66han66@gmail.com>
2023-04-09 23:41:35 +0530

    string and increment operators link

---
+++ Output_questions/string_and_increment_operators.js
@@ -0,0 +1,17 @@
+let marks1 = "10"
+let marks2 = "12"
+
+marks1 = marks1 + 40
+
+marks2++
+
+++marks2
+
+console.log(marks1)
+
+console.log(marks2)
+
+/**
+ * It's the implementation of ++ and ++, both of these first convert the argument
+to Number and then do a ++.
+ */
