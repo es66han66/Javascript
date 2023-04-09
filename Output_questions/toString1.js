3e8be248e8934c80285c884a3bb3076f71d1e8ef
ESHAN GUPTA <es66han66@gmail.com>
2023-04-09 23:37:42 +0530

    toString usage differently

--- Output_questions/toString1.js
+++ Output_questions/toString1.js
@@ -1,1 +1,12 @@
-console.log(23.toString()) // not allowed like this
+// console.log(23.toString()) // not allowed like this
+
+/**
+ * This is due to language grammar limitation.
+The dot(.) character could mean different things in JS. It can be seen as the member operator, or a decimal, depending on its placement.
+
+In order to make above code work we have to either use parenthesis or an additional dot to make the expression valid.
+ */
+
+console.log(21..toString())
+
+console.log((21).toString())
