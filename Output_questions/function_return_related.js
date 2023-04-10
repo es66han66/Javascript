1a24cbb0d5208df03e1bf3ce6a66a702076a1e51
ESHAN GUPTA <es66han66@gmail.com>
2023-04-10 11:24:00 +0530

    Function related to return statement and global scope

---
+++ Output_questions/function_return_related.js
@@ -0,0 +1,21 @@
+function myFunc() {
+    return eshan = 30
+}
+
+function myFunc1() {
+    let eshan1 = 31
+    return eshan1
+}
+
+console.log(myFunc())
+
+console.log(eshan)
+
+
+console.log(myFunc1())
+
+// console.log(eshan1) // error undefined
+
+/**
+ * // Undeclared eshan will be added to global scrope and 90000 will be returned.
+ */
