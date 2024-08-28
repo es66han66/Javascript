bd2993e583ecf80b7b56308956e7586f7e3b8c12
ESHAN GUPTA <es66han66@gmail.com>
2024-08-28 21:30:24 +0530

    find and find index usage

---
+++ Array_Related/find_and_findIndex_usage.js
@@ -0,0 +1,11 @@
+const arr = [1,2,3]
+
+console.log(arr.find((num => num % 2 === 0)))
+
+console.log(arr.findIndex((num => num % 2 === 0)))
+
+const arr1 = [1,3]
+
+console.log(arr1.find((num => num % 2 === 0)))
+
+console.log(arr1.findIndex((num => num % 2 === 0)))
